0ad7f549288903061006e43dbbfb343daaf99746
0gene68 <yeonghun8309@gmail.com>
2023-12-10 03:08:03 +0900

    ✨ feat:  게시글 모달 구현

--- resources/views/non_logged_in/posts_non_logged_in.blade.php
+++ resources/views/non_logged_in/posts_non_logged_in.blade.php
@@ -50,21 +50,20 @@
       <div id="post-container">
         <select name="team" id="select">
           <option selected>팀을 선택하세요</option>
-          <option name="한신">한신 타이거즈</option>
-          <option name="오릭스">오릭스 버팔로즈</option>
-          <option name="히로시마">히로시마 도요 카프</option>
-          <option name="롯데">치바 롯데 마린즈</option>
-          <option name="DeNA">요코하마 DeNA 베이스타즈</option>
-          <option name="소프트뱅크">후쿠오카 소프트뱅크 호크스</option>
-          <option name="요미우리">요미우리 자이언츠</option>
-          <option name="라쿠텐">도호쿠 라쿠텐 골든이글스</option>
-          <option name="야쿠르트">도쿄 야쿠르트 스왈로즈</option>
-          <option name="세이부">사이타마 세이부 라이온즈</option>
-          <option name="주니치">주니치 드래곤즈</option>
-          <option name="닛폰햄">홋카이도 닛폰햄 파이터즈</option>
+          <option value="한신">한신 타이거즈</option>
+          <option value="오릭스">오릭스 버팔로즈</option>
+          <option value="히로시마">히로시마 도요 카프</option>
+          <option value="롯데">치바 롯데 마린즈</option>
+          <option value="DeNA">요코하마 DeNA 베이스타즈</option>
+          <option value="소프트뱅크">후쿠오카 소프트뱅크 호크스</option>
+          <option value="요미우리">요미우리 자이언츠</option>
+          <option value="라쿠텐">도호쿠 라쿠텐 골든이글스</option>
+          <option value="야쿠르트">도쿄 야쿠르트 스왈로즈</option>
+          <option value="세이부">사이타마 세이부 라이온즈</option>
+          <option value="주니치">주니치 드래곤즈</option>
+          <option value="닛폰햄">홋카이도 닛폰햄 파이터즈</option>
         </select>
         <div id="post_title">
-          <span class="number"></span>
           <span class="team">응원팀</span>
           <span class="title">제목</span>
           <span class="writer">작성자</span>
@@ -72,11 +71,11 @@
         </div>
         @foreach ($posts as $post)
         <div class="post">
-          <span class="number">{{$post->id}}</span>
           <span class="team">{{$post->team}}</span>
           <span class="title">{{$post->title}}</span>
           <span class="writer">{{$post->user_name}}</span>
           <span class="created_at">{{\Carbon\Carbon::parse($post->created_at)->format('Y-m-d')}}</span>
+          <button id="show_post" onclick="openModal('{{$post->title}}', '{{$post->team}}', '{{$post->content}}', '{{$post->user_name}}', '{{$post->created_at}}')">상세</button>
         </div>
         @endforeach
         <br>
@@ -84,6 +83,28 @@
         <a href="/create_post" id="create_post_link">
           <button id="create_post_button">글 작성</button>
         </a> 
+    </div>
+  </div>
+  <div id="modal-background" onclick="closeModal()">
+    <div id="modal" class="modal"> 
+        <div class="modal-content">
+          <div class="post-info">
+            <span id="title"></span>
+            <span id="created_at"></span>
+          </div>
+          <hr>
+          <div class="post-info">
+            <span id="team"></span>
+            <span id="writer"></span>
+          </div>
+          <hr>
+          <h4 id="content"></h4>
+          <hr>
+          <div class="post-info">
+            <button id="post-modify">수정</button>
+            <button id="post-delete">삭제</button>
+          </div>
+        </div>
     </div>
   </div>
   <div id="footer" style="color: white"></div>
