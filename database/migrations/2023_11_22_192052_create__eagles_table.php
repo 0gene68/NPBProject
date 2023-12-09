b9e35b4599f6cd22e65d501937537d016178a169
0gene68 <yeonghun8309@gmail.com>
2023-12-09 10:45:14 +0900

    ✨ feat: 라쿠텐 팀 테이블 생성

---
+++ database/migrations/2023_11_22_192052_create__eagles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('Eagles', function (Blueprint $table) {
+            $table->string('backNumber')->primary();
+            $table->string('name_kr');
+            $table->string('name_jp');
+            $table->string('birthDate');
+            $table->string('throw_bat');
+            $table->string('position');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('_eagles');
+    }
+};
