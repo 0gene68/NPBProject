e5dd31c99911e445e75debef92f5df7198656a69
0gene68 <yeonghun8309@gmail.com>
2023-12-11 00:24:06 +0900

    ✨ feat: 회원가입 시 이름 유효성 검사

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -16,8 +16,9 @@ class ProfileUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'min:2', 'max:255', Rule::unique('users')->ignore(auth()->id())],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'password' => 'required|string|confirmed',
         ];
     }
 }
