ee60e9d0a187c3394de49641509212853cd3cfc2
0gene68 <yeonghun8309@gmail.com>
2023-12-13 03:30:46 +0900

    ✨ feat: 게시글 수정 모달 추가

--- resources/views/logged_in/board_logged_in.blade.php
+++ resources/views/logged_in/board_logged_in.blade.php
@@ -72,11 +72,9 @@
           <span class="title">{{$post->title}}</span>
           <span class="writer">{{$post->user_name}}</span>
           <span class="created_at">{{\Carbon\Carbon::parse($post->created_at)->format('Y-m-d')}}</span>
-          <button id="show_post" onclick="openModal('{{$post->title}}', '{{$post->team}}', '{{$post->content}}', '{{$post->user_name}}', '{{$post->created_at}}')">상세</button>
+          <button id="show_post" onclick="openModal('{{$post->title}}', '{{$post->team}}', '{{$post->content}}', '{{$post->user_name}}', '{{\Carbon\Carbon::parse($post->created_at)->format('Y-m-d')}}')">상세</button>
           @if ($user->name == $post->user_name)
-            <button class="modify_post">
-              <a href="/modify_post" class="modify_post">수정</a>
-            </button>
+            <button class="modify_post" onclick="openModalModify('{{$post->id}}', '{{$post->title}}', '{{$post->team}}', '{{$post->content}}')">수정</button>
             <button type="submit" id="delete_post">삭제</button>
             <form action="/posts/{{$post->id}}" method="POST" id="delete_form">
               @csrf
@@ -92,8 +90,8 @@
         </a> 
       </div>
     </div>
-    <div id="modal-background" onclick="closeModal()">
-      <div id="modal" class="modal"> 
+    <div id="modal-info-background">
+      <div class="modal"> 
         <div class="modal-content">
           <div class="post-info">
             <span id="title"></span>
@@ -107,6 +105,39 @@
           <hr>
           <h4 id="content"></h4>
           <hr>
+        </div>
+      </div>
+    </div>
+    <div id="modal-modify-background">
+      <div class="modal"> 
+        <div class="modal-content">
+          <div id="modify_post_container">
+            <div id="modify-post-title">게시글 수정</div><br>
+            <form action="" method="post" id="modify-form">
+                @csrf
+                @method('PUT')
+                <label for="title">제목</label>
+                <input type="text" name="title" id="modify-title" autocomplete="off" required><br><br>
+                <label for="team">응원팀</label>
+                <select name="team" id="modify-select" required>
+                    <option selected>팀을 선택하세요</option>
+                    <option value="한신">한신 타이거스</option>
+                    <option value="오릭스">오릭스 버팔로즈</option>
+                    <option value="히로시마">히로시마 도요 카프</option>
+                    <option value="롯데">치바 롯데 마린즈</option>
+                    <option value="DeNA">요코하마 DeNA 베이스타즈</option>
+                    <option value="소프트뱅크">후쿠오카 소프트뱅크 호크스</option>
+                    <option value="요미우리">요미우리 자이언츠</option>
+                    <option value="라쿠텐">도호쿠 라쿠텐 골든이글스</option>
+                    <option value="야쿠르트">도쿄 야쿠르트 스왈로즈</option>
+                    <option value="세이부">사이타마 세이부 라이온즈</option>
+                    <option value="주니치">주니치 드래곤즈</option>
+                    <option value="닛폰햄">홋카이도 닛폰햄 파이터즈</option>
+                  </select><br><br>
+                  <textarea name="content" id="modify-content" autocomplete="off" cols="76" rows="15" required></textarea><br>
+                  <input type="submit" value="수정" id="modify-button">
+            </form>
+          </div>
         </div>
       </div>
     </div>
