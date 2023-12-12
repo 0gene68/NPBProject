888ab45d4a81744ae308e166182d7ef45de6453b
0gene68 <yeonghun8309@gmail.com>
2023-12-13 00:06:58 +0900

    💄 style: 마이페이지 글씨체 스타일 추가

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="{{ asset('css/main.css') }}">
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
